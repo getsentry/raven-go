01d0e446acbb3096206e510aa9cb0760b31bd41d
Taylor Trimble <taylor@taylortrimble.com>
2013-12-30 14:35:35 -0600

    Implement passing variadic attribtues

--- writer.go
+++ writer.go
@@ -3,18 +3,12 @@ package raven
 type Writer struct {
 	Client *Client
 	Level  Severity
-	Logger string // Logger name reported to Sentry
+	Logger Logger // Logger name reported to Sentry
 }
 
 // Write formats the byte slice p into a string, and sends a message to
 // Sentry at the severity level indicated by the Writer w.
 func (w *Writer) Write(p []byte) (int, error) {
-	message := string(p)
-
-	packet := NewPacket(message, &Message{message, nil})
-	packet.Level = w.Level
-	packet.Logger = w.Logger
-	w.Client.Capture(packet, nil)
-
+	w.Client.CaptureMessage(string(p), w.Level, w.Logger)
 	return len(p), nil
 }
