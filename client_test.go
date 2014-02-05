41a08769596d8078b7f778a434e7945dc5997816
Matt Robenolt <matt@ydekproductions.com>
2014-02-05 01:24:22 -0800

    Log levels should be strings

--- client_test.go
+++ client_test.go
@@ -24,7 +24,7 @@ func TestPacketJSON(t *testing.T) {
 
 	packet.AddTags(map[string]string{"foo": "foo", "baz": "buzz"})
 
-	expected := `{"message":"test","event_id":"2","project":"1","timestamp":"2000-01-01T00:00:00","level":40,"logger":"com.cupcake.raven-go.logger-test-packet-json","tags":[["foo","bar"],["foo","foo"],["baz","buzz"]],"sentry.interfaces.Message":{"message":"foo"}}`
+	expected := `{"message":"test","event_id":"2","project":"1","timestamp":"2000-01-01T00:00:00","level":"error","logger":"com.cupcake.raven-go.logger-test-packet-json","tags":[["foo","bar"],["foo","foo"],["baz","buzz"]],"sentry.interfaces.Message":{"message":"foo"}}`
 	actual := string(packet.JSON())
 
 	if actual != expected {
