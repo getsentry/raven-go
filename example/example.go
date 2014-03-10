19ed071dcddba71bc960e01489c420cd2c353f3f
Olexandr Shalakhin <olexandr@shalakhin.com>
2014-03-10 21:10:55 +0200

    fix travis build

--- example/example.go
+++ example/example.go
@@ -31,7 +31,11 @@ func main() {
 
 // CheckError sends error report to sentry and records event id and error name to the logs
 func CheckError(err error, r *http.Request) {
-	packet = raven.NewPacket(err.Error(), raven.NewException(err, trace()), raven.NewHttp(r))
+	client, err := raven.NewClient(os.Args[1], map[string]string{"foo": "bar"})
+	if err != nil {
+		log.Fatal(err)
+	}
+	packet := raven.NewPacket(err.Error(), raven.NewException(err, trace()), raven.NewHttp(r))
 	eventID, _ := client.Capture(packet, nil)
 	message := fmt.Sprintf("Error event with id \"%s\" - %s", eventID, err.Error())
 	log.Println(message)
