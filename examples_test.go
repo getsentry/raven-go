01dcefe1a2cebbdc0daecf30467d266dbe7a7576
Olexandr Shalakhin <olexandr@shalakhin.com>
2014-03-16 19:27:49 +0200

    fix tests

--- examples_test.go
+++ examples_test.go
@@ -1,18 +1,26 @@
 package raven
 
 import (
+	"errors"
 	"fmt"
+	"net/http"
 	"log"
 	"os"
 )
 
 func Example() {
 	// ... i.e. raisedErr is incoming error
-	client, err := raven.NewClient(sentryDSN, map[string]string{})
+	raisedErr := errors.New("error message here")
+	sentryDSN := "sentry dsn here"
+	r, err := http.NewRequest("GET", "/", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	packet := raven.NewPacket(raisedErr.Error(), raven.NewException(raisedErr, trace()), raven.NewHttp(r))
+	client, err := NewClient(sentryDSN, map[string]string{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	packet := NewPacket(raisedErr.Error(), NewException(raisedErr, trace()), NewHttp(r))
 	eventID, ch := client.Capture(packet, nil)
 	if err = <-ch; err != nil {
 		log.Fatal(err)
@@ -23,14 +31,14 @@ func Example() {
 
 func ExamplePacket() {
 	// os.Args[1] is sentry DSN string
-	client, err := raven.NewClient(os.Args[1], map[string]string{"foo": "bar"})
+	client, err := NewClient(os.Args[1], map[string]string{"foo": "bar"})
 	if err != nil {
 		log.Fatal(err)
 	}
 	req, _ := http.NewRequest("GET", "http://example.com/foo?bar=true", nil)
 	req.RemoteAddr = "127.0.0.1:80"
 	req.Header = http.Header{"Content-Type": {"text/html"}, "Content-Length": {"42"}}
-	packet := &raven.Packet{Message: "Test report", Interfaces: []raven.Interface{raven.NewException(errors.New("example"), trace()), raven.NewHttp(req)}}
+	packet := &Packet{Message: "Test report", Interfaces: []Interface{NewException(errors.New("example"), trace()), NewHttp(req)}}
 	_, ch := client.Capture(packet, nil)
 	if err = <-ch; err != nil {
 		log.Fatal(err)
