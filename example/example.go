ed4949808ff2b48f6686a05d681863ca25a93c44
Taylor Trimble <taylor@taylortrimble.com>
2014-12-14 18:06:54 -0800

    Fix examples from merge from master
    Remove CheckError example: need to replace with better example that
    makes better use of CaptureError and uses a persistent client.

--- example/example.go
+++ example/example.go
@@ -2,8 +2,7 @@ package main
 
 import (
 	"errors"
-	"fmt"
-	"github.com/getsentry/raven-go"
+	"github.com/thenewtricks/raven-go"
 	"log"
 	"net/http"
 	"os"
@@ -14,7 +13,7 @@ func trace() *raven.Stacktrace {
 }
 
 func main() {
-	client, err := raven.NewClient(os.Args[1], &raven.EventInfo{Tags: []raven.Tag{raven.Tag{"foo", "bar"}}})
+	client, err := raven.NewClient(os.Args[1], &raven.Event{Tags: []raven.Tag{raven.Tag{"foo", "bar"}}})
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -23,23 +22,11 @@ func main() {
 	httpReq.RemoteAddr = "127.0.0.1:80"
 	httpReq.Header = http.Header{"Content-Type": {"text/html"}, "Content-Length": {"42"}}
 
-	event := &raven.EventInfo{Interfaces: []raven.Interface{raven.NewException(errors.New("example"), trace()), raven.NewHttp(httpReq)}}
-	eventID, ch := client.Capture("Test report", event)
+	event := &raven.Event{Interfaces: []raven.Interface{raven.NewException(errors.New("example"), trace()), raven.NewHttp(httpReq)}}
+	eventID, ch := client.CaptureMessage("Test report", event)
 	if err = <-ch; err != nil {
 		log.Fatalln(err)
 	}
 
-	log.Println("sent event successfully:", raven.FormatEventID(eventID))
-}
-
-// CheckError sends error report to sentry and records event id and error name to the logs
-func CheckError(err error, r *http.Request) {
-	client, err := raven.NewClient(os.Args[1], map[string]string{"foo": "bar"})
-	if err != nil {
-		log.Fatal(err)
-	}
-	packet := raven.NewPacket(err.Error(), raven.NewException(err, trace()), raven.NewHttp(r))
-	eventID, _ := client.Capture(packet, nil)
-	message := fmt.Sprintf("Error event with id \"%s\" - %s", eventID, err.Error())
-	log.Println(message)
+	log.Println("sent event successfully:", eventID)
 }
