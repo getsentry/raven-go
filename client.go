3e9d2c3e449a83597cd6d345bab2a582b8d2cb32
Steven On <steven.on@davinci11.com>
2019-05-24 16:18:13 +0800

    Add level/severity support

--- client.go
+++ client.go
@@ -698,6 +698,10 @@ func (client *Client) CaptureMessage(message string, tags map[string]string, int
 	}
 
 	packet := NewPacket(message, append(append(interfaces, client.context.interfaces()...), &Message{message, nil})...)
+	if tags["level"] != "" && Severity(tags["level"]) != ""{
+		packet.Level = Severity(tags["level"])
+	}
+
 	eventID, _ := client.Capture(packet, tags)
 
 	return eventID
@@ -719,6 +723,10 @@ func (client *Client) CaptureMessageAndWait(message string, tags map[string]stri
 	}
 
 	packet := NewPacket(message, append(append(interfaces, client.context.interfaces()...), &Message{message, nil})...)
+	if tags["level"] != "" && Severity(tags["level"]) != ""{
+		packet.Level = Severity(tags["level"])
+	}
+
 	eventID, ch := client.Capture(packet, tags)
 	if eventID != "" {
 		<-ch
@@ -751,6 +759,10 @@ func (client *Client) CaptureError(err error, tags map[string]string, interfaces
 	cause := Cause(err)
 
 	packet := NewPacketWithExtra(err.Error(), extra, append(append(interfaces, client.context.interfaces()...), NewException(cause, GetOrNewStacktrace(cause, 1, 3, client.includePaths)))...)
+	if tags["level"] != "" && Severity(tags["level"]) != ""{
+		packet.Level = Severity(tags["level"])
+	}
+
 	eventID, _ := client.Capture(packet, tags)
 
 	return eventID
@@ -776,6 +788,10 @@ func (client *Client) CaptureErrorAndWait(err error, tags map[string]string, int
 	cause := Cause(err)
 
 	packet := NewPacketWithExtra(err.Error(), extra, append(append(interfaces, client.context.interfaces()...), NewException(cause, GetOrNewStacktrace(cause, 1, 3, client.includePaths)))...)
+	if tags["level"] != "" && Severity(tags["level"]) != ""{
+		packet.Level = Severity(tags["level"])
+	}
+
 	eventID, ch := client.Capture(packet, tags)
 	if eventID != "" {
 		<-ch
@@ -807,12 +823,18 @@ func (client *Client) CapturePanic(f func(), tags map[string]string, interfaces 
 				return
 			}
 			packet = NewPacket(rval.Error(), append(append(interfaces, client.context.interfaces()...), NewException(rval, NewStacktrace(2, 3, client.includePaths)))...)
+			if tags["level"] != "" && Severity(tags["level"]) != ""{
+				packet.Level = Severity(tags["level"])
+			}
 		default:
 			rvalStr := fmt.Sprint(rval)
 			if client.shouldExcludeErr(rvalStr) {
 				return
 			}
 			packet = NewPacket(rvalStr, append(append(interfaces, client.context.interfaces()...), NewException(errors.New(rvalStr), NewStacktrace(2, 3, client.includePaths)))...)
+			if tags["level"] != "" && Severity(tags["level"]) != ""{
+				packet.Level = Severity(tags["level"])
+			}
 		}
 
 		errorID, _ = client.Capture(packet, tags)
@@ -845,12 +867,18 @@ func (client *Client) CapturePanicAndWait(f func(), tags map[string]string, inte
 				return
 			}
 			packet = NewPacket(rval.Error(), append(append(interfaces, client.context.interfaces()...), NewException(rval, NewStacktrace(2, 3, client.includePaths)))...)
+			if tags["level"] != "" && Severity(tags["level"]) != ""{
+				packet.Level = Severity(tags["level"])
+			}
 		default:
 			rvalStr := fmt.Sprint(rval)
 			if client.shouldExcludeErr(rvalStr) {
 				return
 			}
 			packet = NewPacket(rvalStr, append(append(interfaces, client.context.interfaces()...), NewException(errors.New(rvalStr), NewStacktrace(2, 3, client.includePaths)))...)
+			if tags["level"] != "" && Severity(tags["level"]) != ""{
+				packet.Level = Severity(tags["level"])
+			}
 		}
 
 		var ch chan error
